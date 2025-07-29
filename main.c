68eacbab3d86da14f1dcb7c2e58bb288469dfc98
Jotinha <84473198+JotinhaFred@users.noreply.github.com>
2025-07-29 11:56:31 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,39 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-void insere(char);
-char retira();
-typedef struct no{
-    char c;
-    struct no *ant;
-}Pilha;
-Pilha *p,*q,*topo;
+void inserir(int);
+void remover();
+void visualizar();
+void listar();
+void verifica_vazia();
+typedef struct no {
+    int val;
+    struct no *prox;
+}Fila;
+Fila *prim,*q,*p,*fim;
 int main(){
-    p,q,topo = NULL;
-    char s[15];
-    int i = 0;
-    scanf("%s",s);
-    while(s[i]!='\0'){
-        insere(s[i++]);
+    prim,q,p = NULL;
+    int n,op,max = 0;
+    int conta = 0;
+    while(op!=6){   
+        printf("Escolha :\n0 - define tamanho\n1 - inserir elem\n2 - remover elem\n3 - visualizar primeiro elemento\n4 - listar elem\n5 - verificar se esta vazia\n");
+        scanf("%d",&op);
+        switch (op){
+        case 0:
+            system("clear");
+            printf("defina o tamanho da fila\n\n");
+            scanf("%d",&max);
+            
+            break;
+        case 1:
+            system("clear");
+            if(n>=max){
+                printf("Lista cheia\n\n");
+                break;
+            }
+            printf("digite um inteiro\n\n");
+            scanf("%d",&n);
+            inserir(n);
+            conta++;
+            
+            break;
+        case 2:
+            system("clear");
+            remover();
+            conta--;
+            break;
+        case 3:
+            system("clear");
+            visualizar();
+            break;
+        case 4:
+            system("clear");
+            listar();
+            break;
+        case 5:
+            system("clear");
+            verifica_vazia();
+            break;
+        default:
+            break;
+        }
     }
-    while(topo!=NULL){ 
-        printf("%c",retira());
-    }
+    
 }
-void insere(char c){
-    p = (Pilha *)malloc(sizeof(Pilha));
-    topo = p;
-    p->c = c;
-    p->ant = q;
+void inserir(int val){
+    p = (Fila*) malloc(sizeof(Fila));
+    
+    p->val = val;
+    p->prox = NULL;
+    if(q!=NULL)
+        q->prox = p;
+    
+    if(prim==NULL){
+        prim = p;
+    }
     q = p;
+    fim = p;
 }
-char retira(){
-    p = topo;
-    char c;
-    if(p!=NULL){
-        topo = p->ant;
-        c = p->c;
-        free(p);
-        return c;
+void remover(){
+    if(prim!=NULL){
+        printf("%d\n\n",prim->val);
+        
+        prim = prim->prox;
+        
     }
+    else{
+        printf("Lista vazia\n\n");
+    }
+    
 }
+void visualizar(){
+    if(prim!=NULL){
+        printf("%d\n\n",prim->val);
+    }
+    
+}
+void listar(){
+    if(prim!=NULL){
+        p = prim;
+    }
+    else{
+        printf("Lista vazia\n\n");
+        return;
+    }
+    while(p!=NULL){
+        printf("%d ",p->val);
+        p = p->prox;
+    }
+    printf("\n\n");
+}
+void verifica_vazia(){
+    if(prim==NULL){
+        printf("Lista esta vazia\n\n");
+        return;
+    }
+    printf("Lista nao esta vazia\n\n");
+}
+
+
+
+//0) Definir o tamanho da fila
+//1) Inserir um elemento na fila
+//2) Remover um elemento da fila
+//3) Visualizar o primeiro elemento da fila (frente da fila)
+//4) Exibir todos os elementos da fila
+//5) Verificar se a fila está vazia ou cheia (caso utilize vetor fixo)
+//6) Sair do programa
